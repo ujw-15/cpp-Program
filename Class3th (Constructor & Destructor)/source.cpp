3333d5c890f3e138b5d6f0b50a81c0456d9fee99
uw15-boop <umsunggyu@gmail.com>
2026-05-19 19:39:20 +0900

    (2026,5,19) 상속

---
+++ Class3th (Constructor & Destructor)/source.cpp
@@ -0,0 +1,145 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Soldier
+{
+private:
+	int health = 0;
+	int defense = 0;
+
+	static int count;
+
+public:
+	Soldier()
+	{
+		cout << "Created Soldier" << endl;
+	}
+
+	~Soldier()
+	{
+		count++;
+
+		cout << "Destruction Count : " << count << endl;
+
+	}
+
+};
+
+int Soldier::count = 0;
+
+class Sprite
+{
+private:
+	int* pixels = nullptr;
+
+public:
+	Sprite()
+	{
+		if (pixels == nullptr)
+		{
+			pixels = new int(1024);
+		}
+
+	}
+
+	Sprite(const Sprite& clone)
+	{
+		pixels = clone.pixels;
+	}
+
+	~Sprite()
+	{
+		delete pixels;
+	}
+
+};
+
+class Mesh
+{
+private:
+	float* vertices = nullptr;
+
+public:
+
+	Mesh()
+	{
+		if (vertices == nullptr)
+		{
+			vertices = new float(512.0f);
+		}
+	}
+
+	Mesh(const Mesh& clone)
+	{
+		vertices = new float(*clone.vertices);
+
+	}
+
+	~Mesh()
+	{
+		delete vertices;
+	}
+};
+
+int main()
+{
+#pragma region 생성자
+	// 클래스의 인스턴스가 생성되는 시점에 자동으로
+	// 호출되는 특수한 멤버 함수입니다.
+
+	// Soldier soldier;
+
+
+	// 생성자의 경우 객체가 생성될 때 단 한 번만 호출되며,
+	// 생성자는 반환형이 존재하지 않기 때문에 생성자가
+	// 호출되기 전에 객체에 대한 메모리는 할당되지 않습니다.
+#pragma endregion
+
+#pragma region 소멸자
+	// 객체가 소멸될 때 자동으로 실행되는 클래스의
+	// 특수한 멤버 함수입니다.
+
+	// Soldier* address[3];
+	// 
+	// 
+	// for (int i = 0; i < 3; i++)
+	// {
+	// 	address[i] = new Soldier;
+	// }
+	// 
+	// for (int i = 0; i < 3; i++)
+	// {
+	// 	delete address[i];
+	// }
+
+	// 소멸자는 객체가 메모리에서 해제될 때 단 한 번만
+	// 호출되며, 소멸자에는 매개 변수를 생성하여 사용할
+	// 수 없습니다.
+#pragma endregion
+
+#pragma region 얕은 복사
+	// 객체를 복사할 때 주소 값을 복사하여 같은
+	// 메모리 공간을 가리키게 하는 복사입니다.
+
+	// Sprite sprite;
+	// 
+	// Sprite copied(sprite);
+
+	// 얕은 복사의 경우 같은 객체가 서로 같은 메모리 공간을
+	// 참조하고 있기 때문에 하나의 객체로 값을 변경하게 되면
+	// 서로 참조된 객체도 함께 영향을 받습니다.
+#pragma endregion
+
+#pragma region 깊은 복사
+	// 객체를 복사할 때, 참조 값이 아닌 객체 자체로 새로
+	// 복사하여 서로 다른 메모리를 생성하는 복사입니다.
+
+	// Mesh mesh;
+	// Mesh duplicate(mesh);
+
+#pragma endregion
+
+	return 0;
+}
+
---
+++ Class4th (Inheritance)/source.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Consumable
+{
+protected:
+	const char* name;
+
+public:
+	Consumable()
+	{
+		cout << "Created Consumable" << endl;
+	}
+
+	~Consumable()
+	{
+		cout << "Destruction Consumable" << endl;
+	}
+};
+
+class Potion : public Consumable
+{
+private:
+	int stemina;
+
+public:
+	Potion()
+	{
+		cout << "Created Potion" << endl;
+	}
+	~Potion()
+	{
+		cout << "Destruction Potion" << endl;
+	}
+};
+
+int main()
+{
+#pragma region 상속
+	// 상위 클래스의 속성을 하위 클래스가 사용할 수 있도록
+	// 설정하는 기능입니다.
+
+	// Consumable consumable;
+	// Potion potion;
+	// 
+	// cout << "Consumable Size : " << sizeof(consumable) << endl;
+	// cout << "Potion Size : " << sizeof(potion) << endl;
+
+	// 클래스의 상속 관계에서 상위 클래스는 하위 클래스의 속성을
+	// 사용할 수 없으며, 하위 클래스는 상위 클래스의 메모리가 포함
+	// 된 상태로 메모리의 크기가 결정됩니다.
+#pragma endregion
+
+
+	return 0;
+}
+
