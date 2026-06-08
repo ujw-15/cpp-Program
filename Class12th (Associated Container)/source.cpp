cbc2d218ee2efad2c6f069216d3e522d34a064d7
ujw-15 <umsunggyu@gmail.com>
2026-06-08 19:47:04 +0900

    (2026,6,8) 반복자

---
+++ Class12th (Associated Container)/source.cpp
@@ -0,0 +1,148 @@
+﻿#include "../Program/stdafx.h"
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+
+int main()
+{
+#pragma region 연관 컨테이너
+	// 특정한 방식으로 데이터를 저장하고 검색하는 컨테이너입니다.
+
+#pragma region set
+	//	std:: set<const char*> set;
+	//	set.insert("wuwa");
+	//	set.insert("Limbus");
+	//	set.insert("NIKKE");
+	//	set.insert("StarRail");
+	//	set.insert("Overwatch");
+	//	set.insert("BlackSoul");
+	//	set.insert("Honkai");
+	//	
+	//	for (const auto& element : set)
+	//	{
+	//		cout << element << endl;
+	//	}
+	//	
+	//	cout << "Set empty : " << set.empty() << endl;
+	//	
+	//	set.clear();
+	//	
+	//	cout << "Set size : " << set.max_size() << endl;
+
+
+#pragma endregion
+
+#pragma region map
+
+	//	std::map <std::string, int> map;
+	//	
+	//	map.insert({ "NPC-1", 75 });
+	//	map.insert({ "NPC-2", 25 });
+	//	map.insert({ "NPC-3", 30 });
+	//	map.insert({ "NPC-4", 50 });
+	//	string name;
+	//	
+	//	cin >> name;
+	//	
+	//	if (map.count(name) > 0)
+	//	{
+	//		map[name] += 10;
+	//	
+	//		for (auto& pair : map)
+	//		{
+	//			if (pair.first != name)
+	//			{
+	//				pair.second -= 10;
+	//			}
+	//	
+	//			cout << pair.first << ":" << pair.second << "\n" << endl;
+	//		}
+	//	}
+	//	else
+	//	{
+	//		cout << name << " : dose not exist in the data.\n" << endl;
+	//	}
+
+#pragma endregion
+
+#pragma region unordered set
+
+	//	unordered_set<const char*> unordered_set;
+	//	
+	//	unordered_set.reserve(16);
+	//	
+	//	unordered_set.max_load_factor(2.0f);
+	//	
+	//	unordered_set.insert("Doran's Ring");
+	//	unordered_set.insert("Doran's Blade");
+	//	unordered_set.insert("Doran's Shield");
+	//	unordered_set.insert("Frozen Heart");
+	//	unordered_set.insert("Sterak's Gage");
+	//	unordered_set.insert("Randuin's Omen");
+	//	unordered_set.insert("Knight's Vow");
+	//	unordered_set.insert("Essence Reaver");
+	//	unordered_set.insert("Liandry's Torment");
+	//	
+	//	cout << "Load Factor : " << unordered_set.load_factor() << endl;
+	//	cout << "Bueckt Count : " << unordered_set.bucket_count() << endl;
+	//	
+	//	if (unordered_set.find("Doran's Shield") != unordered_set.end())
+	//	{
+	//		cout << "the data exists..." << endl;
+	//	}
+	//	else
+	//	{
+	//		cout << "the data does not exits..." << endl;
+	//	}
+	//	unordered_set.erase("Knight's Vow");
+	//	
+	//	for (const char* element : unordered_set)
+	//	{
+	//		cout << element << endl;
+	//	}
+
+#pragma endregion
+
+#pragma region unordered map
+
+//    unordered_map<std::string, int> unordered_map;
+//    
+//    unordered_map["Potion"] = 2;
+//    unordered_map["Elixir"] = 1;
+//    unordered_map["Antidote"] = 2;
+//    unordered_map["Oil"] = 3;
+//    
+//    string name;
+//    
+//    std::cin >> name;
+//    
+//    if (unordered_map.find(name) != unordered_map.end() && unordered_map[name] > 0)
+//    {
+//        unordered_map[name]--;
+//    
+//        cout << name << " use " << endl;
+//    
+//        if (unordered_map[name] <= 0)
+//        {
+//            unordered_map.erase(name);
+//    
+//            cout << name << "All the " << name << " has been used" << endl;
+//        }
+//    }
+//    else
+//    {
+//        cout << "Doesn't Exist." << endl;
+//    }
+//    
+//    for (const auto & element : unordered_map)
+//    {
+//        cout << "Name : " << element.first << " - Quantity : " << element.second << endl;
+//    }
+
+#pragma endregion
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,37 +1,7 @@
 ﻿#include "stdafx.h"
-#include <set>
 
 int main()
 {
-#pragma region 연관 컨테이너
-	// 특정한 방식으로 데이터를 저장하고 검색하는 컨테이너입니다.
-
-#pragma region set
-	//	std:: set<const char*> set;
-	//	set.insert("wuwa");
-	//	set.insert("Limbus");
-	//	set.insert("NIKKE");
-	//	set.insert("StarRail");
-	//	set.insert("Overwatch");
-	//	set.insert("BlackSoul");
-	//	set.insert("Honkai");
-	//	
-	//	for (const auto& element : set)
-	//	{
-	//		cout << element << endl;
-	//	}
-	//	
-	//	cout << "Set empty : " << set.empty() << endl;
-	//	
-	//	set.clear();
-	//	
-	//	cout << "Set size : " << set.max_size() << endl;
-	
-
-#pragma endregion
-
-	// 다중 상속 , 다이아몬드 상속, 가상 상속
-#pragma endregion
 
 	return 0;
 }
